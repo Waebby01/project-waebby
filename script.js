941535f7ecb0c3cdc4bab93720cf7b345f37469b
Waebb01 <villarealmarcosjoaquin44@gmail.com>
2026-03-01 19:24:35 +0800

    update

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
-const openBtn = document.getElementById("openContact");
-const closeBtn = document.getElementById("closeContact");
-const panel = document.getElementById("contactPanel");
 
-openBtn.addEventListener("click", function(e){
-    e.preventDefault();
-    panel.classList.add("active");
+document.addEventListener("DOMContentLoaded", function(){
+
+    const openBtn = document.getElementById("openContact");
+    const closeBtn = document.getElementById("closeContact");
+    const panel = document.getElementById("contactPanel");
+
+    if(openBtn){
+        openBtn.addEventListener("click", function(e){
+            e.preventDefault();
+            panel.classList.add("active");
+        });
+    }
+
+    if(closeBtn){
+        closeBtn.addEventListener("click", function(){
+            panel.classList.remove("active");
+        });
+    }
+
 });
+const revealElements = document.querySelectorAll(".about-section, .stat-box, .skill");
 
-closeBtn.addEventListener("click", function(){
-    panel.classList.remove("active");
+window.addEventListener("scroll", () => {
+    revealElements.forEach(el => {
+        const windowHeight = window.innerHeight;
+        const elementTop = el.getBoundingClientRect().top;
+
+        if(elementTop < windowHeight - 100){
+            el.style.opacity = "1";
+            el.style.transform = "translateY(0)";
+        }
+    });
 });
-
-function openContact(){
-    document.getElementById("contactPanel").classList.add("active");
-}
-
-function closeContact(){
-    document.getElementById("contactPanel").classList.remove("active");
-}
